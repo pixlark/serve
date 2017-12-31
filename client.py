f6977675b985530ed91b83488f6b81c87db73adb
pixlark <t1000.pixlark@gmail.com>
2017-12-31 16:54:59 -0500

    basic chatting

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import socket as sk
+import threading as th
 
 ip = '127.0.0.1'
 port = 5005
@@ -8,7 +9,26 @@ buffer_size = 1024
 
 sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 sock.connect((ip, port))
-sock.send(message.encode())
-response = sock.recv(buffer_size)
-print('Received response: ', response)
+
+def send_loop():
+	while True:
+		msg = input('> ')
+		if (msg.lower() == 'quit' or msg.lower() == 'q'):
+			sock.send(b'QUIT')
+			break
+		sock.send(b'MESG' + msg.encode())
+
+def listen_loop():
+	while True:
+		data = sock.recv(1024)
+		print(data.decode() + '\n> ', end='')
+
+send_thread   = th.Thread(target=send_loop)
+listen_thread = th.Thread(target=listen_loop, daemon=True)
+
+send_thread.start()
+listen_thread.start()
+
+send_thread.join()
+
 sock.close()
--- server.py
+++ server.py
@@ -7,12 +7,11 @@ import time
 
 sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 sock.setsockopt(sk.SOL_SOCKET, sk.SO_REUSEADDR, 1)
-sock.bind(('127.0.0.1', 5005))
+sock.bind(('0.0.0.0', 5005))
 sock.listen(5)
 
-quit()
-
 clients = []
+threads = []
 
 clients.append(sock.accept())
 print('First client connected from', clients[0][1])
@@ -23,12 +22,24 @@ def manage_client(client, addr):
 	print('Listening to messages from', addr)
 	while True:
 		data = client.recv(1024)
-		print(data)
+		print('HEADER', data[:4])
+		if (data[:4] == b'MESG'):
+			print(str(addr) + ':', data[4:])
+			for c in clients:
+				if (c[1] == addr):
+					continue
+				c[0].send(data[4:])
+		elif (data[:4] == b'QUIT'):
+			print(str(addr), 'has quit the conversation')
+			return
 
 for c in clients:
-	client_thread = th.Thread(target=manage_client, args=c)
-	client_thread.start()
+	threads.append(th.Thread(target=manage_client, args=c))
+	threads[-1].start()
 
+for t in threads:
+	t.join()
+	
 for c in clients:
 	c[0].close()
 
