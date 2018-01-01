a6b0235f536a5a44dee225a3ec7e5a1f3511c95c
pixlark <t1000.pixlark@gmail.com>
2017-12-31 21:23:48 -0500

    work

--- server.py
+++ server.py
@@ -20,10 +20,7 @@ print('Listening on port', port)
 clients = []
 threads = []
 
-clients.append(sock.accept())
-print('First client connected from', clients[0][1])
-clients.append(sock.accept())
-print('Second client connected from', clients[1][1])
+running = True
 
 def manage_client(client, addr):
 	print('Listening to messages from', addr)
@@ -40,12 +37,22 @@ def manage_client(client, addr):
 			print(str(addr), 'has quit the conversation')
 			return
 
-for c in clients:
-	threads.append(th.Thread(target=manage_client, args=c))
-	threads[-1].start()
+def listen_for_new_clients():
+	while True:
+		clients.append(sock.accept())
+		print(clients[-1][1], 'has joined the conversation')
+		threads.append(th.Thread(target=manage_client, args=c))
+		threads[-1].start()
 
-for t in threads:
-	t.join()
+listen_thread = th.Thread(target=listen_for_new_clients)
+listen_thread.start()
+
+while True:
+	i = input('')
+	print(i)
+	if (i.lower() == 'quit' or i.lower() == 'q'):
+		running = False
+		break
 	
 for c in clients:
 	c[0].close()
