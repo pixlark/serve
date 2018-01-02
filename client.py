9aa9ed348e4a6d32159a6cd6247f8a55f7695a59
pixlark <t1000.pixlark@gmail.com>
2018-01-01 19:52:40 -0500

    changes

--- client.py
+++ client.py
@@ -3,6 +3,9 @@
 import socket as sk
 import threading as th
 import configparser as cr
+import select as sl
+import sys
+import time
 
 config = cr.ConfigParser()
 config.read('cfg')
@@ -13,25 +16,36 @@ port = int(config['client']['port'])
 sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 sock.connect((ip, port))
 
+running = True
+
 def send_loop():
 	while True:
 		msg = input('> ')
-		if (msg.lower() == 'quit' or msg.lower() == 'q'):
+		if msg.lower() == 'quit' or msg.lower() == 'q':
 			sock.send(b'QUIT')
 			break
 		sock.send(b'MESG' + msg.encode())
 
 def listen_loop():
+	global running
 	while True:
-		data = sock.recv(1024)
-		print(data.decode() + '\n> ', end='')
+		if sl.select([sock], [], [])[0]:
+			data = sock.recv(1024)
+		else:
+			time.sleep(0.05)
+			continue
+		if (data[:4] == b'MESG'):
+			print(data[4:].decode() + '\n> ', end='')
+		elif (data[:4] == b'DOWN'):
+			print('The server has shut down.')
+			return
 
-send_thread   = th.Thread(target=send_loop)
-listen_thread = th.Thread(target=listen_loop, daemon=True)
+listen_thread = th.Thread(target=listen_loop)
+send_thread   = th.Thread(target=send_loop, daemon=True)
 
-send_thread.start()
 listen_thread.start()
+send_thread.start()
 
-send_thread.join()
+listen_thread.join()
 
 sock.close()
--- server.py
+++ server.py
@@ -5,11 +5,15 @@ import socket as sk
 import threading as th
 import time
 import configparser as cr
+import select as sl
 
 config = cr.ConfigParser()
 config.read('cfg')
 port = int(config['server']['port'])
 print('Loaded configuration')
+
+sk.setdefaulttimeout(float(config['server']['timeout']))
+print('Timeout set to', config['server']['timeout'])
 
 sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 sock.setsockopt(sk.SOL_SOCKET, sk.SO_REUSEADDR, 1)
@@ -23,9 +27,13 @@ threads = []
 running = True
 
 def manage_client(client, addr):
-	print('Listening to messages from', addr)
-	while True:
-		data = client.recv(1024)
+	#print('Listening to messages from', addr)
+	while running:
+		if (len(sl.select([client], [], [])[0]) > 0):
+			data = client.recv(1024)
+		else:
+			time.sleep(1)
+			continue
 		print('HEADER', data[:4])
 		if (data[:4] == b'MESG'):
 			print(str(addr) + ':', data[4:])
@@ -38,10 +46,13 @@ def manage_client(client, addr):
 			return
 
 def listen_for_new_clients():
-	while True:
-		clients.append(sock.accept())
+	while running:
+		try:
+			clients.append(sock.accept())
+		except sk.timeout:
+			continue
 		print(clients[-1][1], 'has joined the conversation')
-		threads.append(th.Thread(target=manage_client, args=c))
+		threads.append(th.Thread(target=manage_client, args=clients[-1]))
 		threads[-1].start()
 
 listen_thread = th.Thread(target=listen_for_new_clients)
@@ -49,9 +60,11 @@ listen_thread.start()
 
 while True:
 	i = input('')
-	print(i)
 	if (i.lower() == 'quit' or i.lower() == 'q'):
+		print('Shutting down...')
 		running = False
+		for c in clients:
+			c[0].send('DOWN'.encode())
 		break
 	
 for c in clients:
