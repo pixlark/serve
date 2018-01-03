3051b64d424ef2573006bf5fda24d075d91d03a8
pixlark <t1000.pixlark@gmail.com>
2018-01-02 19:06:13 -0500

    basic functionality

--- client.py
+++ client.py
@@ -7,6 +7,10 @@ import select as sl
 import sys
 import time
 
+if len(sys.argv) < 2:
+	print('Provide a username please')
+	quit()
+
 config = cr.ConfigParser()
 config.read('cfg')
 
@@ -15,37 +19,40 @@ port = int(config['client']['port'])
 
 sock = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
 sock.connect((ip, port))
+sock.send(b'JOIN' + sys.argv[1].encode())
 
 running = True
 
 def send_loop():
-	while True:
-		msg = input('> ')
+	global running
+	while running:
+		msg = input('')
 		if msg.lower() == 'quit' or msg.lower() == 'q':
 			sock.send(b'QUIT')
-			break
+			running = False
 		sock.send(b'MESG' + msg.encode())
 
 def listen_loop():
 	global running
-	while True:
-		if sl.select([sock], [], [])[0]:
-			data = sock.recv(1024)
-		else:
-			time.sleep(0.05)
-			continue
+	while running:
+		data = sock.recv(1024)
 		if (data[:4] == b'MESG'):
-			print(data[4:].decode() + '\n> ', end='')
+			print(data[4:].decode())
 		elif (data[:4] == b'DOWN'):
-			print('The server has shut down.')
-			return
+			print('The server has shut down')
+			running = False
+		elif (data[:4] == b'QUIT'):
+			print('{} has quit'.format(data[4:].decode()))
+		elif (data[:4] == b'JOIN'):
+			print('{} has joined'.format(data[4:].decode()))
 
-listen_thread = th.Thread(target=listen_loop)
+listen_thread = th.Thread(target=listen_loop, daemon=True)
 send_thread   = th.Thread(target=send_loop, daemon=True)
 
 listen_thread.start()
 send_thread.start()
 
-listen_thread.join()
+while running:
+	pass
 
 sock.close()
--- server.py
+++ server.py
@@ -26,23 +26,32 @@ threads = []
 
 running = True
 
-def manage_client(client, addr):
-	#print('Listening to messages from', addr)
-	while running:
-		if (len(sl.select([client], [], [])[0]) > 0):
+def manage_client(client, addr, username):
+	while True:
+		if len(sl.select([client], [], [])[0]) > 0:
 			data = client.recv(1024)
 		else:
 			time.sleep(1)
 			continue
 		print('HEADER', data[:4])
-		if (data[:4] == b'MESG'):
-			print(str(addr) + ':', data[4:])
+		if data[:4] == b'MESG':
+			print(username.decode() + ':', data[4:].decode())
 			for c in clients:
-				if (c[1] == addr):
+				if c[1] == addr:
 					continue
-				c[0].send(data[4:])
-		elif (data[:4] == b'QUIT'):
-			print(str(addr), 'has quit the conversation')
+				c[0].send(data[:4] + username + b': ' + data[4:])
+				print('Forwarded to', c[2].decode())
+		elif data[:4] == b'QUIT':
+			print(username.decode(), 'has quit')
+			ri = -1
+			for i in range(len(clients)):
+				if clients[i][1] == addr:
+					ri = i
+				else:
+					clients[i][0].send(b'QUIT' + username)
+			if (ri == -1):
+				raise RuntimeError('Something went wrong')
+			del clients[ri]
 			return
 
 def listen_for_new_clients():
@@ -51,8 +60,12 @@ def listen_for_new_clients():
 			clients.append(sock.accept())
 		except sk.timeout:
 			continue
-		print(clients[-1][1], 'has joined the conversation')
-		threads.append(th.Thread(target=manage_client, args=clients[-1]))
+		username= clients[-1][0].recv(1024)[4:]
+		clients[-1] = (clients[-1][0], clients[-1][1], username)
+		print(clients[-1][2], 'has joined')
+		for c in clients:
+			c[0].send(b'JOIN' + username)
+		threads.append(th.Thread(target=manage_client, args=clients[-1], daemon=True))
 		threads[-1].start()
 
 listen_thread = th.Thread(target=listen_for_new_clients)
@@ -60,7 +73,7 @@ listen_thread.start()
 
 while True:
 	i = input('')
-	if (i.lower() == 'quit' or i.lower() == 'q'):
+	if i.lower() == 'quit' or i.lower() == 'q':
 		print('Shutting down...')
 		running = False
 		for c in clients:
